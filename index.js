4ed4a60370f94681f4256da6ce4ce1c738a5a015
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-01-31 01:57:12 +0530

    Added Logic for Adding salary and expense forms and displaying them accordingly

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ let balance;
 let income;
 let expense;
 
+let count = 1000;
+let srCount = 1;
 
-let container = document.querySelector('.container');
+let container = document.querySelector(".container");
 // Salary Add Money and Salary Form Data -
 const addMoneyButton = document.querySelector(".add-money-btn");
 
@@ -20,9 +22,7 @@ const enterTransaction = document.querySelector(".enter-transaction");
 const enterTransactionDetails = document.querySelector(
   ".enter-transaction-details"
 );
-// let paidItem = document.querySelector('#paid-item').innerHTML;
-// let paidAmount = document.querySelector('#paid-amount').innerHTML;
-// console.log(paidItem, paidAmount);
+
 const transactionCloseButton = document.querySelector(".btn-close");
 const ExpenseTrackerBtn = document.querySelector(".expense-tracker-btn");
 
@@ -30,11 +30,6 @@ const salaryCredited = document.querySelector(".salary-credited");
 console.log(salaryCredited);
 const salaryDiscredited = document.querySelector(".salary-discredited");
 console.log(salaryDiscredited);
-
-// const paidItemValue = document.querySelector('#paid-item').innerHTML;
-// console.log(paidItemValue);
-// const paidAmountValue = document.querySelector('#paid-amount').innerHTML;
-// // console.log(paidAmountValue);
 
 // ! Add Money Modal -
 // When the user clicks on addMoneyButton, open the modal
@@ -67,12 +62,16 @@ transactionCloseButton.addEventListener("click", function (e) {
 
 addSalaryForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  srCount++;
+
+  // Function for creating salary Div 
+
+  createSalaryCreditedDiv();
+  console.log("After the createSalaryDiscredited!");
+
   // getting the input value from input filed
   let inputValue = Number(document.querySelector("input").value);
   console.log(inputValue);
-
-  // Adding the input value to inner html
-  // console.log( balance.length);
 
   if (!balance) {
     console.log("inside this if block");
@@ -95,31 +94,22 @@ addSalaryForm.addEventListener("submit", function (e) {
   }
 
   // Adding the total value in inner html..and displaying it as salary credited div
-  createSalaryCreditedDiv();
-  // document.querySelector("#amount-added").innerHTML = income;
+  document.querySelector(`#salary-span-${srCount}`).innerHTML = 'Salary';
+  document.querySelector(`#salary-amount-${srCount}`).innerHTML = income;
 
   // Show and hide the forms
   ExpenseTrackerBtn.style.display = "block"; // Show the plus icon
   addSalaryForm.style.display = "none";
-  salaryCredited.style.display = "flex";
+  document.querySelector(`#id-${srCount}`).style.display = "flex";
+
   inputValue = "";
   console.log(inputValue);
 });
 
-const createSalaryCreditedDiv = function() {
-  let salaryCreditedDiv = `
-  <div class="salary-credited">
-            <span>salary</span>
-            <span>cr</span>
-            <span id="amount-added">${income}</span>
-        </div>`; 
-  // salaryCreditedDiv.style.display = 'flex';
-        console.log('salaryCreditedDiv', salaryCreditedDiv);
-        document.querySelector(".container").appendChild(salaryCreditedDiv);
-}
 
 addTransactionForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  count ++;
   console.log("Inside addTransactionForm submit event listener");
 
   // Call the createSalaryDiscreditedDiv() everytime user submits the addTransactionForm..
@@ -128,7 +118,6 @@ addTransactionForm.addEventListener("submit", function (e) {
   // getting the input value from input filed
   let transactionValue = enterTransaction.value;
   let transactionDetailsValue = enterTransactionDetails.value;
-  console.log(transactionValue, transactionDetailsValue);
 
   // Subtracting the balance amount with the expense amount
 
@@ -136,145 +125,53 @@ addTransactionForm.addEventListener("submit", function (e) {
   addMoneyButton.innerHTML = balance;
   // Adding the input value to innerHTML
 
-  if (document.querySelector(`#paid-item`).innerHTML === "") {
-    document.querySelector(`#paid-item`).innerHTML = transactionDetailsValue;
-    document.querySelector(`#paid-amount`).innerHTML = transactionValue;
-  } else {
-    document.querySelector(
-      `#paid-item-${
-        document.querySelector(".salary-discredited").children.length
-      }`
-    ).innerHTML = transactionDetailsValue;
-    document.querySelector(
-      `#paid-amount-${
-        document.querySelector(".salary-discredited").children.length
-      }`
-    ).innerHTML = transactionValue;
-    document.querySelector(
-      `#paid-item-${
-        document.querySelector(".salary-discredited").children.length
-      }`
-    ).parentElement.style.display = "flex";
-    console.log(
-      "inside paid-item-id",
-      document.querySelector(
-        `#paid-item-${
-          document.querySelector(".salary-discredited").children.length
-        }`
-      )
-    );
-  }
+  document.querySelector(`#paid-item-${count}`).innerHTML = transactionDetailsValue;
+  document.querySelector(`#paid-amount-${count}`).innerHTML = transactionValue;
 
   // Making the input values null
   enterTransaction.value = "";
   enterTransactionDetails.value = "";
-  // Display hide or show
-  // salaryDiscredited.style.setProperty('display', 'flex');
-  // addTransactionForm.style.setProperty('display', 'none');
-  // console.log(salaryDiscredited);
-  document.querySelector(".salary-discredited").style.display = "flex";
 
+  // Display hide or show
+  document.querySelector(`#id-${count}`).style.display = "flex";
   addTransactionForm.style.display = "none";
-  document.querySelector(".salary-discredited").children+ 1;
 });
 
 // Create SalaryDiscreditedDiv and add class, span tags etc to it..
+
 const createSalaryDiscreditedDiv = function () {
-  console.log("We are inside the createSalaryDiscreditedDiv function!");
   let salaryDiscreditedDiv = document.createElement("div");
-  console.log("salaryDiscreditedDiv is credited");
   salaryDiscreditedDiv.setAttribute("class", "salary-discredited");
+  salaryDiscreditedDiv.setAttribute("id", `id-${count}`);
   document.querySelector(".container").appendChild(salaryDiscreditedDiv);
 
-  // Logic for looping through - 
-
-  // for(let i =0; i< )
-
-  // Logic for appending children in correct salaryDiscreditedDiv -
-
-  if (document.querySelector(".salary-discredited").children.length === 0) {
-    salaryDiscreditedDiv.setAttribute(
-      "id",
-      `id-${document.querySelector(".salary-discredited").children.length}`
-    );
-    let paidItemSpan = document.createElement("span");
-    paidItemSpan.setAttribute("id", `paid-item`);
-    // paidItemSpan.innerHTML = 'paid'
-    document.querySelector(".salary-discredited").appendChild(paidItemSpan);
-    console.log("Added paid item span");
-    let drSpan = document.createElement("span");
-    drSpan.innerHTML = "dr";
-    document.querySelector(".salary-discredited").appendChild(drSpan);
-    console.log("Added dr span");
-    let paidAmountSpan = document.createElement("span");
-    // paidAmountSpan.innerHTML = '0'
-    paidAmountSpan.setAttribute("id", `paid-amount`);
-    document.querySelector(".salary-discredited").appendChild(paidAmountSpan);
-  } else if (
-    document.querySelector(".salary-discredited").children.length > 0
-  ) {
-    salaryDiscreditedDiv.setAttribute(
-      "id",
-      `id-${document.querySelector(".salary-discredited").children.length}`
-    );
-    let paidItemSpan = document.createElement("span");
-    paidItemSpan.setAttribute(
-      "id",
-      `paid-item-${
-        document.querySelector(".salary-discredited").children.length
-      }`
-    );
-
-    // paidItemSpan.innerHTML = 'paid'
-    document
-      .querySelector(
-        `#id-${document.querySelector(".salary-discredited").children.length}`
-      )
-      .appendChild(paidItemSpan);
-    console.log("Added paid item span");
-    let drSpan = document.createElement("span");
-    drSpan.innerHTML = "dr";
-    document
-      .querySelector(
-        `#id-${document.querySelector(".salary-discredited").children.length}`
-      )
-      .appendChild(drSpan);
-    console.log("Added dr span");
-    let paidAmountSpan = document.createElement("span");
-    // paidAmountSpan.innerHTML = '0'
-    paidAmountSpan.setAttribute(
-      "id",
-      `paid-amount-${
-        document.querySelector(".salary-discredited").children.length
-      }`
-    );
-
-    document
-      .querySelector(
-        `#id-${document.querySelector(".salary-discredited").children.length}`
-      )
-      .appendChild(paidAmountSpan);
-  }
-
-  // for(let i =0; document.querySelector('.salary-discredited').children.length; i++ ) {
-  // salaryDiscreditedDiv.setAttribute("id", `id-${document.querySelector('.salary-discredited').children.length}`);
-  //   console.log('Inside the legendary for loop');
-  // }
-
-  // let paidItemSpan = document.createElement("span");
-  // paidItemSpan.setAttribute("id", "paid-item");
-  // // paidItemSpan.innerHTML = 'paid'
-  // document.querySelector(".salary-discredited").appendChild(paidItemSpan);
-  // console.log('Added paid item span');
-  // let drSpan = document.createElement("span");
-  // drSpan.innerHTML = "dr";
-  // document.querySelector(".salary-discredited").appendChild(drSpan);
-  // console.log('Added dr span');
-  // let paidAmountSpan = document.createElement("span");
-  // // paidAmountSpan.innerHTML = '0'
-  // paidAmountSpan.setAttribute("id", "paid-amount");
-  // document.querySelector(".salary-discredited").appendChild(paidAmountSpan);
-  //   console.log('paid amount span');
-  //   console.log(salaryDiscreditedDiv.children.length);
-  //   console.log(salaryDiscreditedDiv)
+  let paidItemSpan = document.createElement("span");
+  paidItemSpan.setAttribute("id", `paid-item-${count}`);
+  document.querySelector(`#id-${count}`).appendChild(paidItemSpan);
+  let drSpan = document.createElement("span");
+  drSpan.innerHTML = "dr";
+  document.querySelector(`#id-${count}`).appendChild(drSpan);
+  let paidAmountSpan = document.createElement("span");
+  paidAmountSpan.setAttribute("id", `paid-amount-${count}`);
+  document.querySelector(`#id-${count}`).appendChild(paidAmountSpan);
 };
+
+
+// Create SalaryDiscreditedDiv and add class, span tags etc to it..
+
+const createSalaryCreditedDiv = function() {
+  let salaryCreditedDiv = document.createElement("div");
+  salaryCreditedDiv.setAttribute("class", "salary-credited");
+  salaryCreditedDiv.setAttribute("id", `id-${srCount}`);
+  document.querySelector(".container").appendChild(salaryCreditedDiv);
+
+  let salarySpan = document.createElement("span");
+  salarySpan.setAttribute("id", `salary-span-${srCount}`);
+  document.querySelector(`#id-${srCount}`).appendChild(salarySpan);
+  let crSpan = document.createElement("span");
+  crSpan.innerHTML = "cr";
+  document.querySelector(`#id-${srCount}`).appendChild(crSpan);
+  let salaryAmountSpan = document.createElement("span");
+  salaryAmountSpan.setAttribute("id", `salary-amount-${srCount}`);
+  document.querySelector(`#id-${srCount}`).appendChild(salaryAmountSpan);
+}
